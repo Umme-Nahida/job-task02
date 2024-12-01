1b8b9f93e5d0f45910767d9c196f29a2bef0022f
Developer Nahida <ummenahida20@gmail.com>
2024-12-02 00:14:15 +0600

    create assets folder added images

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary:'#287279',
+        secondary:'#9998E1'
       },
     },
   },
