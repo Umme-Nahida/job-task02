42b4db2f002c0aafacc9b28fe2c7e5971b4b3355
Developer Nahida <ummenahida20@gmail.com>
2024-12-02 16:31:22 +0600

    create footer with responsive

--- app/Components/Footer/page.js
+++ app/Components/Footer/page.js
@@ -5,52 +5,44 @@ const Footer = () => {
     return (
         <footer className="py-20 bg-gray-100">
             <Container>
-                <div className='flex flex-col lg:flex-row justify-evenly items-start  gap-x-40 mb-10'>
+                <div className='flex flex-col lg:flex-row lg:justify-evenly items-start  gap-x-40 mb-10 px-10 lg:px-0'>
                     {/* category */}
                     <ul className='flex flex-col  space-y-2 '>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
+                        <li className='mb-3 font-bold'>Category</li>
+                        <li>Nutrition and diet</li>
+                        <li>Mindfulness</li>
+                        <li>Mental health</li>
+                        <li>Personal growth</li>
+                        <li>Social well-being</li>
                     </ul>
 
                     {/* about */}
                     <ul className='flex flex-col  space-y-2 '>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
+                        <li className='mb-3 font-bold'>About</li>
+                        <li>About us</li>
+                        <li>Our partners</li>
+                        <li>Investors</li>
+                        <li>Career</li>
                     </ul>
 
                     {/* Resources */}
                     <ul className='flex flex-col  space-y-2 '>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
+                        <li className='mb-3 font-bold'>Resources</li>
+                        <li>FAQ</li>
+                        <li>Refund policies</li>
+                        <li>Terms and conditions</li>
+                        <li>Cookie</li>
+                        <li>Latest posts</li>
                     </ul>
 
                     {/* support */}
                     <ul className='flex flex-col  space-y-2 '>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
-                        <li>Category</li>
+                        <li className='mb-3 font-bold'>Support</li>
+                        <li>Get in touch</li>
+                        <li>Visit our forum</li>
                     </ul>
                 </div>
-                <div className="flex flex-col items-center justify-around gap-5 bg-gray-300  dark:bg-gray-500 dark:text-white md:flex-row md:gap-0 py-5 border-dashed border-y-2 border-gray-500">
+                <div className=" flex flex-col lg:flex-row justify-center items-center lg:justify-between dark:bg-gray-500 dark:text-white py-5 border-dashed border-y-2 border-gray-500">
                     <h5 className="text-2xl font-bold">NavigateUI</h5>
                     <nav className="text-lg">
                         <ul className="flex h-full items-center justify-between gap-3">
@@ -66,7 +58,7 @@ const Footer = () => {
                         </ul>
                     </nav>
                 </div>
-                <aside className="bg-gray-500 pl-1 py-5 text-left text-sm text-white dark:bg-gray-800 ">
+                <aside className=" py-5 text-center lg:text-left text-sm dark:bg-gray-800 ">
                     <p>&copy; 2024 NavigateUI. All Rights Reserved.</p>
                 </aside>
             </Container>
